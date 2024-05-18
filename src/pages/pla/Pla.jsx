c338ef9ffba108df68d44ed22df39d391c5e253c
Michele <mikzed@hotmail.it>
2024-05-17 17:29:41 -0700

    pls project

--- src/pages/pla/Pla.jsx
+++ src/pages/pla/Pla.jsx
@@ -19,8 +19,99 @@ function Pla() {
       <Navbar />
       <div className="pla">
         <div className="container">
-          <h2>PLA</h2>
-          <p>ppppppp</p>
+          <h2>PLA: The Presidential Leadership Academy</h2>
+          <p>
+            The Presidential Leadership Academy (PLA) is a three-year leadership
+            and critical thinking program offered to select students at Penn
+            State. Students can apply their freshman year, and 30 individuals
+            are selected amongst the candidates. The PLA provides opportunities
+            such as classes with University leadership, trips across the US, and
+            the ability to engage in high-level debate. This page reflects on
+            some selected experiences Ian had in the academy.
+          </p>
+          <div className="split">
+            <div className="left">
+              <h3>TRIPS</h3>
+              <h4>WASHINGTON D.C.</h4>
+              <p>
+                My first trip with the PLA was to our nation's capital. Here we
+                got to tour the monuments, explore the Holocaust museum, and
+                even visit George Washington's estate at Mount Vernon. We also
+                got to chat with PLA alumni, including my sister Alyssa
+                Sihelnik. I especially liked when we got a private tour of the
+                Smithsonian.
+              </p>
+            </div>
+            <h4>CHICAGO</h4>
+            <p>
+              Chicago was a grand adventure of a trip. We learned about some of
+              the famous architecture created by Frank Lloyd Wright. We also got
+              to tour a waste water treatment plant, which was an amazing
+              engineering feat. My favorite part of this trip was our visit to
+              the McDonald's headquarters, since I used to work at the golden
+              arches for five years!
+            </p>
+            <h4>BALTIMORE</h4>
+            <p>
+              Baltimore was my last fall trip with the PLA. Here we got to learn
+              about a second hand store called Second Chance that hires people
+              who traditional could not find work due to criminal or drug
+              backgrounds. We also got to visit the Baltimore Museum of Art. We
+              also went on a boat cruise and watched an air show taking place.
+              It was on this cruise that I mentioned to our director, Melissa
+              Doberstein, that our next trip should be to Los Angeles... and it
+              actually came true! A funny memory from this trip was when a
+              couple other PLA members and I went to a haunted house inside a
+              Mall outside of the city. It was really well done!
+            </p>
+            <h4>LOS ANGELES</h4>
+            <p>
+              LOS ANGELES I may be biased, since I was the one to suggest this
+              location, however I stand by my claim that this is the best PLA
+              trip. From going up to the Griffith Observatory, to our food tour
+              with Six Taste, we all had an amazing time. We got to experience
+              both modern and contemporary art through our visits to The Broad
+              and The Getty. One of the most interesting parts of our trip was
+              the visit to The Ronald Reagan Library, a place that I would not
+              have gone to if it weren't for the PLA. Between shopping in
+              Beverly Hills with some friends, to perusing the boardwalk in
+              Santa Monica, this trip will always have a special place in my
+              heart!
+            </p>
+          </div>
+          <div className="right">
+            <h3>BLOGS</h3>
+            <h4>MINOR: ENVRIONMENTAL ENGINEERING</h4>
+            <p>
+              A requirement of the PLA is that students must blog about either
+              in-class experience (From HONOR 201, 301, or 401 courses), or blog
+              about other topics of interest in their busy lives. All of Ian's
+              publicly available blogs can be viewed HERE, however select
+              favorites are described below:
+            </p>
+            <div className="btn">
+              Modern Thermodynamics and the Origins of Life
+            </div>
+            <p>
+              This blog discusses how thermodynamics can play a rather unique
+              role in helping answer some of life's most profound questions.{" "}
+            </p>
+            <div className="btn">
+              When Virtual Reality Makes a Better Reality
+            </div>
+            <p>
+              This post explores how excess waste can be avoided using VR
+              technology, and that it can offer more to humanity than being
+              unsustainable ever did.
+            </p>
+            <div className="btn">Gratitude</div>
+            <p>
+              This entry talks about how a relatively simple New Year's
+              resolution can have a lasting impact on one's overall happiness.
+            </p>
+            <h3>REFLECTIONS ON PLA</h3>
+            <p>---------------LIST HERE---------------</p>
+          </div>
         </div>
       </div>
       <Footer />
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -20,7 +20,80 @@ function Projects() {
       <div className="projects">
         <div className="container">
           <h2>PROJECTS</h2>
-          <p>ppppppp</p>
+          <div className="split">
+            <div className="left">
+              <h3>CHEMICAL-LOOPING COMBUSTION</h3>
+              <h4>WASTE RESOURCE RECOVERY RESEARCH</h4>
+              <p>
+                Spring 2018 to Spring 2020 - As a part of the requirements for
+                graduation in the Energy and Mineral Engineering master's
+                program, Ian conducted research under the guidance of Dr. Sarma
+                Pisupati. He focused on how petroleum coke ash, a waste product
+                of the fossil fuel industry, can be reformatted and used as an
+                oxygen carrier in chemical-looping combustion (CLC). CLC
+                provides the benefits of creating a virtually 100% capture and
+                sequestration-ready supply of CO2. By combining the research
+                areas of advanced combustion and waste resource recovery, Ian
+                hopes to pave the way for future sustainable power generation.
+              </p>
+              <div className="btn">
+                Chemical-looping combustion presentation
+              </div>
+            </div>
+            <h3>EUROPA CLIPPER SPACECRAFT</h3>
+            <h4>THERMAL ENGINEERING INTERNSHIP</h4>
+            <p>
+              Summer 2019 - Following his junior year, Ian returned to the NASA
+              Jet Propulsion Laboratory (JPL) for a second internship. At JPL,
+              he worked with the thermal engineering team for the Europa Clipper
+              spacecraft. He was responsible for overseeing the tests of newly
+              constructed thin film heaters that were to be installed at various
+              points on the spacecraft. Utilizing advanced thermal modeling
+              software, as well as performing watt density tests in a vacuum
+              chamber, Ian established maximum power limits for the heaters
+              which helped to guide the design principle used for the mission.
+              He also got to work under Jenny Hua, who is an amazing mentor!
+            </p>
+            <div className="btn">
+              Europa Clipper thermal engineering presentation
+            </div>
+          </div>
+          <div className="right">
+            <h3>SHELL ECO-MARATHON</h3>
+            <h4>URBAN CONCEPT VEHICLE CAPSTONE</h4>
+            <p>
+              Spring 2019 - As a part of his senior design project, Ian
+              participated on the Shell Eco-Marathon Urban Concept Vehicle team
+              at Penn State. Working on the powertrain team, Ian's specific
+              responsibilities included: managing the fuel system, assisting
+              with welding and manufacturing of critical components, and
+              creating the vehicle's exhaust subsystem. The Eco-marathon team
+              completed their vehicle in a nine week timespan, and successfully
+              competed with it at the Sonoma Raceway in California. The vehicle
+              achieved a fuel efficiency of 238.1 mpg, and did not crash once!
+              Ian believes this was the best team project he ever got to work on
+              at Penn State.
+            </p>
+            <div className="btn">Eco-marathon vehicle presentation</div>
+            <h3>ENCELADUS CONCEPT MISSION</h3>
+            <h4>SYSTEMS ENGINEERING INTERNSHIP</h4>
+            <p>
+              Summer 2018 - After his sophomore year, Ian received his first
+              summer internship with the Systems Engineering division at the
+              NASA Jet Propulsion Laboratory (JPL). There, he was placed on a
+              discovery-class mission concept team, working on a proposal for a
+              spacecraft destined to explore Enceladus, an icy moon of Saturn.
+              At JPL, Ian got to know Shaun Standley, who was his mentor for the
+              10-week-long internship. Although the proposal was cut a few weeks
+              after Ian returned to Penn State, the fantastic experience and
+              newly obtained skills allowed Ian to pursue a second internship at
+              JPL for the following summer.
+            </p>
+            <div className="btn">Enceladus mission concept presentation*</div>
+            *Because of the private nature of information in this presentation,
+            please enter Ian's PSU Access ID to view presentation. Not for
+            public access.
+          </div>
         </div>
       </div>
       <Footer />
