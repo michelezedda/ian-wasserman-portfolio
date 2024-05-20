b5368f472e1cdb37665128293f3a86b525c07e47
Michele <mikzed@hotmail.it>
2024-05-20 15:23:38 -0700

    header and more

--- src/components/more/More.jsx
+++ src/components/more/More.jsx
@@ -14,18 +14,21 @@ function More() {
               <div>
                 <img src={educationPic} alt="Education" />
                 <h4>EDUCATION</h4>
+                <p>Undergraduate and Graduate Studies</p>
               </div>
             </Link>
             <Link to="projects">
               <div>
                 <img src={projectsPic} alt="Projects" />
                 <h4>PROJECTS</h4>
+                <p>Various Technical Endeavors</p>
               </div>
             </Link>
             <Link to="pla">
               <div>
                 <img src={plaPic} alt="PLA" />
                 <h4>PLA</h4>
+                <p>Presidential Leadership Academy</p>
               </div>
             </Link>
           </div>
